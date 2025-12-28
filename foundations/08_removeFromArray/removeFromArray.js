0bf65d41fb90cd5d696426c3a4b51b81579dc97b
claudere <129028339+claudere0@users.noreply.github.com>
2025-12-29 00:27:14 +0500

    loops and arrays exercies complited

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let resultArray = arr;
+
+    const removeArgFromArray = function(arr, arg) {
+        let newArray = [];
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] !== arg) {
+                newArray.push(arr[i]);
+            } 
+        }
+        return newArray;
+    };
+
+    for (let arg of args) {
+        resultArray = removeArgFromArray(resultArray, arg);
+    }
+
+    return resultArray;
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    let result = 0;
 
+    if (!Number.isInteger(a) || !Number.isInteger(b)) {
+        return "ERROR";
+    }
+
+    if (((a || b) < 0)){
+        return "ERROR";
+    }
+    
+    if (a<b){
+        for (let i = a; i <= b; i++) {
+            result+=i;
+        }
+    } else if (a>b){
+        for (let i = a; i >= b; i--) {
+            result+=i;
+        }
+    }
+
+    return result;
 };
 
 // Do not edit below this line
