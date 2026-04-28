0a8b24121bca79a55fcb703e028cf5d222622a18
claudere <129028339+claudere0@users.noreply.github.com>
2026-04-28 22:55:29 +0500

    calculator completed

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let total = 0;
+  for (let item in arr) {
+    total += arr[item];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let total = 1;
+  for (let item in arr) {
+    total *= arr[item];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a,n) {
+	result = 1
+  for (let i = 0; i < n; i++){
+    result *= a
+  }
+  return result 
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	result = 1
+  for (let i = 1; i <= n; i++){
+    result *= i
+  }
+  return result 
 };
 
 // Do not edit below this line
