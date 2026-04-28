60ba2cf5c4936c753c786a0468557b7a4d622d84
claudere <129028339+claudere0@users.noreply.github.com>
2026-04-28 23:10:53 +0500

    palindromes done

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    str = str.replace(/[^a-z0-9]/g, '');
+    let reverse = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        reverse +=str[i];
+    }
+    return str === reverse;
 };
 
 // Do not edit below this line
