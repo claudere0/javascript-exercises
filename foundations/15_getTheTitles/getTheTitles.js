9db1ee60771647b1c760be643a34727944f6c645
claudere <129028339+claudere0@users.noreply.github.com>
2026-04-28 23:41:03 +0500

    getTheTitles one

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,22 @@
-const getTheTitles = function() {
+// const books = [
+//     {
+//       title: 'Book',
+//       author: 'Name'
+//     },
+//     {
+//       title: 'Book2',
+//       author: 'Name2'
+//     }
+// ]
 
+const getTheTitles = function(objArr) {
+    let titleArr = []
+    for (let obj of objArr) {
+        titleArr.push(obj.title)
+    }
+    return titleArr;
 };
 
+// console.log(getTheTitles(books))
 // Do not edit below this line
 module.exports = getTheTitles;
